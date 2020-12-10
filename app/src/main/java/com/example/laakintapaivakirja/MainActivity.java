650deb122960bccc57e84136463cf4daa23ffa31
suvilaak <Suvi.Laakkonen2@metropolia.fi>
2020-12-10 21:46:23 +0200

    commit

--- app/src/main/java/com/example/laakintapaivakirja/MainActivity.java
+++ app/src/main/java/com/example/laakintapaivakirja/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvShowNameAndAge;
