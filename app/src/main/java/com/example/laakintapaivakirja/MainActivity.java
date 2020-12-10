0613489d5f3b25ddd3f677162acbf48cadfebca4
suvilaak <Suvi.Laakkonen2@metropolia.fi>
2020-12-10 16:08:53 +0200

    turhien poistoa

--- app/src/main/java/com/example/laakintapaivakirja/MainActivity.java
+++ app/src/main/java/com/example/laakintapaivakirja/MainActivity.java
@@ -3,14 +3,8 @@ package com.example.laakintapaivakirja;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,26 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController); */
     }
 
     public void buttonPressed(View view) {
-
-        if(view == findViewById(R.id.buttonProfile)) {
+        if (view == findViewById(R.id.buttonProfile)) {
             Intent profile = new Intent(this, Profile.class);
             startActivity(profile);
-        }
-
-        if(view == findViewById(R.id.button)) {
-
         }
     }
 }
