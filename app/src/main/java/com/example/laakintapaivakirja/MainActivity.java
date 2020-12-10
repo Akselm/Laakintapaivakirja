3457efa2a619cf7452acaa75d16b970833c7c449
suvilaak <Suvi.Laakkonen2@metropolia.fi>
2020-12-10 20:04:02 +0200

    kommentoitu

--- app/src/main/java/com/example/laakintapaivakirja/MainActivity.java
+++ app/src/main/java/com/example/laakintapaivakirja/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.laakintapaivakirja;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvShowNameAndAge;
+    private TextView textView;
+    private final String shredPreferencesName = "MessageStore";
+    private final String messageKey = "LastValue";
     public static final String EXTRA = "com.example.laakintapaivakirja";
 
     @Override
@@ -15,12 +24,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Profiiliaktiviteetissa tallennettu nimi tulee näkyviin.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Profile.EXTRA_MESSAGE);
+        textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+
+        SharedPreferences prefGet = getSharedPreferences(shredPreferencesName, Context.MODE_PRIVATE);
+        textView.setText(prefGet.getString(messageKey,""));
     }
 
+    /**
+     * Nappia painamalla aukeaa uusi aktiviteetti.
+     * @param view määrittelee mikä id toteutetaan.
+     */
     public void buttonPressed(View view) {
         if (view == findViewById(R.id.buttonProfile)) {
             Intent profile = new Intent(this, Profile.class);
             startActivity(profile);
         }
+    }
+
+    /**
+     * Ohjelma tallentaa käyttäjän asettamat tiedot vaikka sovellus suljetaan.
+     */
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefPut = getSharedPreferences(shredPreferencesName, Activity.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = prefPut.edit();
+
+        prefEditor.putString(messageKey, textView.getText().toString());
+        prefEditor.commit();
     }
 }
--- app/src/main/java/com/example/laakintapaivakirja/Profile.java
+++ app/src/main/java/com/example/laakintapaivakirja/Profile.java
@@ -12,11 +12,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * Luokka sisältää käyttäjäprofiilin tarvitsemia tietoja.
+ * @author Suvi Laakkonen
+ * @version 10/12/2020
+ */
+
 public class Profile extends AppCompatActivity {
 
     private TextView tvShowNameAndAge;
     private final String shredPreferencesName = "MessageStore";
     private final String messageKey = "LastValue";
+    public static final String EXTRA_MESSAGE = "com.example.laakintapaivakirja.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +35,19 @@ public class Profile extends AppCompatActivity {
 
         tvShowNameAndAge = findViewById(R.id.tvShowNameAndAge);
 
+        //Tallennaetaan käyttäjän asettama nimi muistiin, jos ei anneta sisältöä, ei tulosteta mitään näkyviin.
         SharedPreferences prefGet = getSharedPreferences(shredPreferencesName, Context.MODE_PRIVATE);
         tvShowNameAndAge.setText(prefGet.getString(messageKey,""));
     }
 
+    /**
+     * Nappia painamalla asetettu nimi ja ikä tallentuvat laitteelle ja palataan main aktiviteettiin.
+     * @param view määrittelee mikä id toteutetaan.
+     */
     public void buttonPressed(View view) {
         if(view == findViewById(R.id.buttonOK)) {
 
+            //Muunnetaan molempien editText kenttien sisällöt merkkijonoiksi.
             EditText etName = (EditText) findViewById(R.id.editTextName);
             EditText etAge = (EditText) findViewById(R.id.editTextAge);
 
@@ -44,10 +57,18 @@ public class Profile extends AppCompatActivity {
             String strName = etName.getText().toString();
             String strAge = etAge.getText().toString();
 
-            tvShowNameAndAge.setText(strName + " , " + strAge);
+            //Asetetaan käyttäjän nimi näkyville ok -nappiaa painamalla ja viedään annettu nimi main aktiviteettiin.
+            tvShowNameAndAge.setText("Hei " + strName + "!");
+
+            Intent intentMain = new Intent(this, MainActivity.class);
+            intentMain.putExtra(EXTRA_MESSAGE, strName);
+            startActivity(intentMain);
         }
     }
 
+    /**
+     * Ohjelma tallentaa käyttäjän asettamat tiedot vaikka sovellus suljetaan.
+     */
     protected void onPause() {
         super.onPause();
 
