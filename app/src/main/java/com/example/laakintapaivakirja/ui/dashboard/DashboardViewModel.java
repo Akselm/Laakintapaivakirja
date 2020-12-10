7c44e8a00b435a3dc3d07257a123b020f47c1536
suvilaak <Suvi.Laakkonen2@metropolia.fi>
2020-12-10 11:09:07 +0200

    päivitystä

--- app/src/main/java/com/example/laakintapaivakirja/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/laakintapaivakirja/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("User profile");
+        mText.setValue("Käyttäjäprofiili");
     }
 
     public LiveData<String> getText() {
